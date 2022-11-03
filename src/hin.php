cb2d7c3f83bc1bffddade718aefc06ba9194d24e
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2022-11-03 20:55:22 +0530

    Create hin.php

---
+++ src/hin.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+header("content-type: video/mp2t");
+header("pragma: no-cache");
+$ts = $_GET['ts'];
+$tok = file_get_contents("getmekey/token.txt");
+$pxy = "";
+$url = $ts;
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Referer: https://gocast2.com"
+  )
+);
+$context = stream_context_create($opts);
+$e = file_get_contents($url, false, $context);
+echo $e;
+?>
