5941ef281723ddfdc0941a2cb86bee4fdf9809c0
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-22 11:56:51 +0530

    Update watchnow.php

--- src/watchnow.php
+++ src/watchnow.php
@@ -5,7 +5,7 @@ $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
 //for debug only!
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
