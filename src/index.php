10b5cbc5815ca8d1cf05e5ed1f0cb076acbeb3d6
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-23 22:59:57 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,15 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-08PMTQRVH5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-08PMTQRVH5');
+</script>
     <link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />	
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     
