2b16ba792c3415bb604b5355b5fefe8eba22e977
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-23 23:00:39 +0530

    Update player.php

--- src/player.php
+++ src/player.php
@@ -3,6 +3,15 @@
 
 
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-08PMTQRVH5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-08PMTQRVH5');
+</script>
 
 <title>WATCH-IT</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
