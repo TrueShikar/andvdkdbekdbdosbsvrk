e493f4c4c7f8b869932d9de9f9049c1de5037938
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-27 22:54:39 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -119,18 +119,100 @@
     <td>Sony Sports Ten 5 (formerly Sony Six) is an Indian English-language pay television sports channel operated by Culver Max Entertainment</td>
     <td><a href = "sportsplayer.php?v=jvjmt31JC920190929">Play Link</a></td>
   </tr>
+  <tr>
     <td>SONY ESPN</td>
     <td>Sony ESPN was an Indian pay television sports channel which was a joint venture between ESPN Inc. and Sony Pictures Networks India</td>
     <td><a href = "sportsplayer.php?v=YNuzz6acZy20190929">Play Link</a></td>
   </tr>
 
+  <tr>
+    <td>SKY SPORTS MAIN EVENT</td>
+    <td>Sky Sports Main Event - live scores, news results, highlights, videos, photos, and fixtures for International and County matches.</td>
+    <td><a href = "sportsplayer.php?v=skysme">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SKY SPORTS ACTION</td>
+    <td>Sky Sports ACTION offers advertisers a unique opportunity to target and reach sports fans with coverage of Rugby League, Rugby Union and more.</td>
+    <td><a href = "sportsplayer.php?v=skysact">Play Link</a></td>
+  </tr>
+
+  <tr>
+    <td>SKY SPORTS GOLF</td>
+    <td>Latest Golf news from Sky Sports. European tour and PGA tour covered along with Masters, Open, US Open, PGA Championships and Ryder Cup.</td>
+    <td><a href = "sportsplayer.php?v=skysgola">Play Link</a></td>
+  </tr>
+
+  <tr>
+    <td>SKY SPORTS ARENA</td>
+    <td>The channel that brings you closer to live sport, including all of the action from International and European Rugby Union, Rugby League, Boxing, GAA, and Darts</td>
+    <td><a href = "sportsplayer.php?v=skysare">Play Link</a></td>
+  </tr>
+
+  <tr>
+    <td>SKY SPORTS F1</td>
+    <td>Sky Sports F1 is a television channel created exclusively for Sky's UK and Ireland coverage of Formula One, with Sky having a package of rights from the 2012 season to the 2029 season.</td>
+    <td><a href = "sportsplayer.php?v=skysfor1">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SKY SPORTS MIX</td>
+    <td>Sky Sports Mix is a sports channel from Sky which launched in Summer 2016 showing sports coverage from the other Sky Sports channels, as well as minority sports.</td>
+    <td><a href = "sportsplayer.php?v=skysmixx">Play Link</a></td>
+  </tr>
+
+  <tr>
+    <td>SKY SPORTS NEWS</td>
+    <td>Sky Sports News is the home of breaking sports news.</td>
+    <td><a href = "sportsplayer.php?v=skyspnews">Play Link</a></td>
+  </tr>
+
+  <tr>
+    <td>BT SPORTS 1</td>
+    <td>the channel has aired football games, rugby matches, hockey games, cricket clashes, motor sport events and UFC fights.</td>
+    <td><a href = "sportsplayer.php?v=bbtsp1">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>BT SPORTS 2</td>
+    <td>2nd chann of BT SPORTS</td>
+    <td><a href = "sportsplayer.php?v=bbtsp2">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>BT SPORTS 3</td>
+    <td>3rd chann of BT SPORTS</td>
+    <td><a href = "sportsplayer.php?v=bbtsp3">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>BT SPORTS 4</td>
+    <td>4th chann of BT SPORTS</td>
+    <td><a href = "sportsplayer.php?v=bbtespn">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>EURO SPORTS 1</td>
+    <td>Eurosport 1 is a French television sports network channel which is a division of Eurosport and a subsidiary of Warner Bros.</td>
+    <td><a href = "sportsplayer.php?v=eurosp1">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>EURO SPORTS 2</td>
+    <td>Eurosport 2 describes itself as "the new generation sports channel", dedicated to team sports, alternative sports, discovery and entertainment including basketball</td>
+    <td><a href = "sportsplayer.php?v=eurosp2">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>VIAPLAY-1</td>
+    <td>Viaplay Sports and Premier Sports are a group of pay television sports channels owned by Viaplay Group and SSBL Limited.</td>
+    <td><a href = "sportsplayer.php?v=premieruk">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>VIAPLAY-2</td>
+    <td>Viaplay is a video streaming service owned by Swedish media company Viaplay Group</td>
+    <td><a href = "sportsplayer.php?v=hdchnl2">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>VIAPLAY XTRA</td>
+    <td>Viaplay Xtra launched as FreeSports on 31 August 2017, with the aim to provide free-to-air sports coverage and become a top three dedicated sports channel in the UK.</td>
+    <td><a href = "sportsplayer.php?v=hdchnl3">Play Link</a></td>
+  </tr>
 
 </body>
 </html>
-
-
-
-
 
 
 
