3f6f1c172f77f17d6d70257a611c44847c301e2b
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-29 00:46:46 +0530

    Update sportsplayer.php

--- src/sportsplayer.php
+++ src/sportsplayer.php
@@ -3,7 +3,6 @@
 
 
 <head>
-<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5833762,document.body||document.documentElement)</script>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-08PMTQRVH5"></script>
 <script>
