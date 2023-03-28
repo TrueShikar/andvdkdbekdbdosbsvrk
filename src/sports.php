e554e0d8cc4198eb71a2782f34f4a732f323c710
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-28 13:56:17 +0530

    Add files via upload

--- src/sports.php
+++ src/sports.php
@@ -1,13 +1,13 @@
 <?php
 
 $v= $_GET['v'];
-$url= "https://gocast2.com/crichdws.php?player=mobile&live=".$v;
+$url= "https://123ecast.xyz/embed2.php?v=".$v."&vw=100%&vh=100";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $headers = array(
-   "Referer: https://gocast2.com",
+   "Referer: https://crichdplayer.xyz/",
 );
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 //for debug only!
@@ -25,23 +25,21 @@ function lpcode($string, $start, $end){
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
      }
-     $start='return([';
-     $end='].join("")';
+     $start='<source src="';
+     $end='"';
      $myoutput=lpcode($resp,$start,$end);
 
-$shikari = str_replace('"','',$myoutput);
-$shikari1 = str_replace(',','',$shikari);
-$shikari2 = stripslashes($shikari1);
+$shikari = str_replace($v.'.m3u8','',$myoutput);
 
 
 //new curl
 
-$curl1 = curl_init($shikari2);
-curl_setopt($curl1, CURLOPT_URL, $shikari2);
+$curl1 = curl_init($myoutput);
+curl_setopt($curl1, CURLOPT_URL, $myoutput);
 curl_setopt($curl1, CURLOPT_RETURNTRANSFER, true);
 
 $headers1 = array(
-   "Referer: https://gocast2.com",
+   "Referer: https://123ecast.xyz",
 );
 curl_setopt($curl1, CURLOPT_HTTPHEADER, $headers1);
 //for debug only!
@@ -53,7 +51,7 @@ curl_close($curl1);
 
 //hls setup and hls//
 $cc= "?=".$v;
-$elink = "https://sp5.flowerscast.com:999/hls/";
+$elink = $shikari;
 $opts = array(
   'http'=>array(
     'method'=>"GET",
@@ -62,7 +60,7 @@ $opts = array(
 );
 $context = stream_context_create($opts);
 $f = preg_replace("/(?<=ts).*/", "", $resp1);
-$g = preg_replace("/(".$cc.").*ts/", "hin.php?ts=".$elink."$0", $f);
+$g = preg_replace("/(".$cc.").*ts/", "hin1.php?ts=".$elink."$0", $f);
 
 
 header("Content-Type: application/vnd.apple.mpegurl");
