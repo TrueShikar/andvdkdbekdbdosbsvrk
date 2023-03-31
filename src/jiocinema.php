a655db35c5d68f78d639fcaf8b4755464121bc63
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-03-31 19:30:23 +0530

    Update jiocinema.php

--- src/jiocinema.php
+++ src/jiocinema.php
@@ -1,4 +1,13 @@
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-08PMTQRVH5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-08PMTQRVH5');
+</script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <script src="//jwpsrv.com/library/O4mPlqeTEeK_viIACpYGxA.js"></script>
