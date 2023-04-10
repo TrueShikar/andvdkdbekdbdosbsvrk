5ee3c8802e9e8100d7ad7dd1a425d7da20d8f391
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-04-10 13:21:59 +0530

    Create macdisney.php

---
+++ src/macdisney.php
@@ -0,0 +1,45 @@
+<?php
+error_reporting(0);
+// create a new cURL resource
+$ch = curl_init();
+
+// set the URL to fetch
+$url = 'http://iptvtree.net:8080/live/asadman2022/63ZFJytXY3oyqe8z/192985.m3u8';
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// set options to include the HTTP headers in the output
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// execute the request and get the response
+$response = curl_exec($ch);
+
+// get the HTTP response code
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// close the cURL resource
+curl_close($ch);
+
+// output the HTTP response headers and body
+$resp = "HTTP/{$httpCode}\n{$response}";
+
+$resp1 = $resp."emd";
+
+
+//lpcode
+function lpcode($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+     }
+     $start="Location: ";
+     $end="emd";
+     $myoutput=lpcode($resp1,$start,$end);
+
+
+
+header("Location: https://ww1.shikaritv.tk/macplayer.php?v=".$myoutput);
+?>
