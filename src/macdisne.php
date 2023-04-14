b6fcb1e7d2b3ce3e467b0ecd31e20cfd86b24f35
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-04-14 09:37:11 +0530

    Update macdisne.php

--- src/macdisne.php
+++ src/macdisne.php
@@ -3,16 +3,13 @@ error_reporting(0);
 header("content-type: video/mp2t");
 header("pragma: no-cache");
 $ts = $_GET['ts'];
-$tok = file_get_contents("getmekey/token.txt");
-$pxy = "";
-$hole = 'http://188.241.218.10:8070/'.$ts;
-$url = $pxy.$hole."?".$tok;
+$hole = 'http://188.241.218.10:8070'.$ts;
 $opts = array(
   'http'=>array(
     'method'=>"GET"
   )
 );
 $context = stream_context_create($opts);
-$e = file_get_contents($url, false, $context);
+$e = file_get_contents($hole, false, $context);
 echo $e;
 ?>
